91a7f94213b30fef3635922c1cdbc6019c4678ba
Eric Hartley <spiffydude0@gmail.com>
2024-10-19 20:45:03 -0500

    completed exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrone(string word)
+        {
+            char[] arr = word.ToCharArray();
+            Array.Reverse(arr);
+            var reversed = new string(arr);
+
+            if (reversed == word)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,24 @@
-using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("palindrome", false)]
+        [InlineData("rotator", true)]
+        [InlineData("demon", false)]
+        [InlineData("supercalifragilisticexpialidocious", false)]
+        public void IsAPalindroneTest(string word, bool expected)
         {
-
+            // Arrange
+            var WordSmith = new WordSmith();
+            // Act
+            var actual = WordSmith.IsAPalindrone(word);
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
